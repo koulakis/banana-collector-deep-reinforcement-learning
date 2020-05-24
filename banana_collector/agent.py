11e24379624f734669fb0b4cebd58c3701bbb7e8
mariosk <marios.koulakis@gmail.com>
2020-05-24 17:33:19 +0200

    Add custom unity agents and notebook which tests random agent.

---
+++ banana_collector/agent.py
@@ -0,0 +1,42 @@
+import random
+from abc import ABC, abstractmethod
+from typing import Optional
+
+import numpy as np
+
+
+class Agent(ABC):
+    """Base class for agents with one dimensional state & action inputs."""
+
+    def __init__(self, state_size: int, action_size: int, seed: Optional[int] = None):
+        """Initialize an Agent object.
+
+        Args:
+            state_size: dimension of each state
+            action_size: dimension of each action
+            seed: random seed
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.seed(seed)
+
+    @abstractmethod
+    def step(self, state: np.ndarray, action: int, reward: float, next_state: np.ndarray, done: bool):
+        pass
+
+    @abstractmethod
+    def act(self, state: np.ndarray) -> int:
+        pass
+
+
+class RandomAgent(Agent):
+    def __init__(self, state_size: int, action_size: int, seed: Optional[int] = None):
+        super().__init__(state_size, action_size, seed)
+
+    def step(self, state: np.ndarray, action: int, reward: float, next_state: np.ndarray, done: bool):
+        """No training for the random agent."""
+        pass
+
+    def act(self, state: np.ndarray) -> int:
+        """Randomly select and return an action."""
+        return random.randint(0, self.action_size)
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from setuptools.command.install import install
+from subprocess import check_call
+
+UNITY_AGENTS_PATH = 'udacity_custom_unity_agents/'
+
+
+class InstallUdacityCustomUnityAgents(install):
+    """Install the agents defined by udacity before setting up the package."""
+    def run(self):
+        check_call(f'pip install {UNITY_AGENTS_PATH}'.split(' '))
+        install.run(self)
+
+
+setuptools.setup(
+    name="banana-collector",
+    version="0.0.1",
+    author="Marios Koulakis",
+    description="This is a solution for the first project of the Udacity deep reinforcement learning course.",
+    packages=['banana_collector', 'scripts'],
+    install_requires=[
+        'tensorflow==1.7.1',
+        'mlagents',
+        'numpy'
+    ],
+    cmdclass={
+        'install': InstallUdacityCustomUnityAgents
+    },
+    extras_require={
+        'dev': [
+            'jupyterlab'
+        ]
+    },
+    python_requires='~=3.6'
+)
