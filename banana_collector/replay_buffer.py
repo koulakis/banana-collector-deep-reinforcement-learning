Correct the beta annealing steps to approximate the average #steps per episode times the number of episodes.

banana_collector/replay_buffer.py
@@ -77,7 +77,7 @@ class PrioritizedReplayBuffer:
             device: torch.device,
             alpha: float = 0.6,
             beta_0: float = 0.4,
-            beta_number_annealing_steps=2000):
+            beta_number_annealing_steps=2000*75):
         """Initialize a ReplayBuffer object.
 
             Args: